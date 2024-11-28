ff2bd3a4a9dc0ae9c137eb22434c2a72a55e834f
Tarabanchuk Ekaterina <summertime.99Fj@gmail.com>
2024-11-28 22:12:18 +0400

    task

---
+++ 8kyu/combine-strings-function.js
@@ -0,0 +1,12 @@
+// Combine strings function
+
+// DESCRIPTION: Create a function named combineNames / combine_names / CombineNames that accepts two parameters(first and last name).
+// The function should return the full name.
+
+// SOLUTION:
+
+function combineNames(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+
+console.log(combineNames('James', 'Stevens'));
