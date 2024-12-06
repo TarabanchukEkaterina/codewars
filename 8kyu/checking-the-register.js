c54e9e257ea50455ca1478d56e63db699f21c9fe
Tarabanchuk Ekaterina <summertime.99Fj@gmail.com>
2024-12-06 23:40:58 +0400

    create function checking the register

---
+++ 8kyu/checking-the-register.js
@@ -0,0 +1,31 @@
+// Is the string uppercase ?
+
+// DESCRIPTION:
+// Is the string uppercase ?
+
+// Create a method to see whether the string is ALL CAPS.
+//   Examples(input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+// In this Kata, a string is said to be in ALL CAPS whenever it does not contain 
+// any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+
+
+// SOLUTION:
+String.prototype.isUpperCase = function () {
+  if (this.toString() === this.toUpperCase()) {
+    return true;
+  }
+  return false;
+}
+console.log("c".isUpperCase());// false
+console.log("C".isUpperCase());// true
+console.log("hello I AM DONALD".isUpperCase());// false
+console.log("HELLO I AM DONALD".isUpperCase());// true
+console.log("ACSKLDFJSgSKLDFJSKLDFJ".isUpperCase());// false
+console.log("ACSKLDFJSGSKLDFJSKLDFJ".isUpperCase());// true
